d72650eafc850a800942859b68c9eeda7d778767
AbdulKhaliq59 <kananuraabdulkhaliq59@gmail.com>
2023-02-07 11:48:09 +0200

    This is documentations for Users API

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,35 @@
 const express = require("express");
 // const User = require("../models/user");
-const userController=require('../controller/user');
+const userController = require("../controller/user");
+const authenticate = require("../middleware/auth");
 // const { get } = require("mongoose");
 const router = express.Router();
 
-//Get All user stored in database
-router.get('/',userController.getAllUser);
-//Save User in database
-router.post('/',userController.createUser);
-//update user 
-router.patch('/:id',userController.updateUser);
-//deleter User
-router.delete('/:id',userController.deleteUser);
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve all users
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Returned Successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   user:
+ *                     type: object
+ *                     description: The user information
+ *       401:
+ *         description: Unauthorized
+ */
 
-module.exports=router;
+//Get All user stored in database
+router.get("/", authenticate, userController.getAllUser);
+module.exports = router;
