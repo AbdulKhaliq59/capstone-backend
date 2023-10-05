60f2deda32cd4d029030c089f3517a9c86bd8f77
AbdulKhaliq59 <kananuraabdukhaliq59@gmail.com>
2023-10-05 21:36:04 +0200

    feat(documentation): Create documentation  - Create Documentation for account verification  - Create Documentation for signin account

--- routes/Auth.js
+++ routes/Auth.js
@@ -5,13 +5,130 @@ const { createUser } = require("../controller/user");
 
 const router = express.Router();
 
+//Documentation for signin
+
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     summary: Sign in a user
+ *     tags: [Auth]
+ *     description: Sign in an existing user with their email and password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The email address of the user
+ *               password:
+ *                 type: string
+ *                 description: The password of the user
+ *     responses:
+ *       201:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: A status indicating success
+ *                 Message:
+ *                   type: string
+ *                   description: A welcome message for the user
+ *                 data:
+ *                   type: string
+ *                   description: The JWT token assigned to the user
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message indicating that the user was not found
+ *                 error:
+ *                   type: string
+ *                   description: Error message indicating that the password is incorrect or the account is not verified
+ *                 auth_error:
+ *                   type: string
+ *                   description: Error message indicating that the user is not authorized
+ */
+
+
 router.post("/signin", userController.signin);
+
+//Documentation for Account verification
+/**
+ * @swagger
+ * /auth/verify_account:
+ *   post:
+ *     summary: Verify a user account
+ *     tags: [Auth]
+ *     description: Verify a user's account by providing a valid token
+ *     parameters:
+ *       - in: query
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The verification token received by the user's email
+ *     responses:
+ *       200:
+ *         description: Account verified successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: A success message indicating that the account has been verified
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message indicating that the token is missing
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message indicating that the token is invalid
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message indicating that the user associated with the token was not found
+ */
+
 router.post("/verify_account", userController.verifyAccount);
 
 //Documentation for signup
 /**
  * @swagger
- * /signup:
+ * /auth/signup:
  *   post:
  *     summary: Create a new user
  *     tags: [Auth]
